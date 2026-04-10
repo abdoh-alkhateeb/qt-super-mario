39f12d0a6dc8ca410ed39320c8ef53ad6d58f1cc
christine selwanes <christineselwanes@aucegypt.edu>
2026-04-10 17:22:59 +0000

    Add two platforms, fix player spawn, add You lost! message box

--- include/player.hpp
+++ include/player.hpp
@@ -1,22 +1,18 @@
 #pragma once
-
 #include <QGraphicsRectItem>
 #include <QKeyEvent>
 #include <QObject>
 
 class Player : public QObject, public QGraphicsRectItem {
   Q_OBJECT
-
  public:
   Player(QGraphicsItem* parent = nullptr);
-
  public slots:
   void updateState();
-
  protected:
   void keyPressEvent(QKeyEvent* event) override;
-
  private:
   int velocityY;
   bool onGround;
+  bool gameOver;
 };
