3a5e2d48dfe74898b4e887f2034da9c679f39ca8
Samoura007 <samer007@lavache.com>
2026-04-10 06:13:11 +0200

    I've add the 2 blocks, Sir Samer

--- src/player.cpp
+++ src/player.cpp
@@ -25,17 +25,21 @@ void Player::keyPressEvent(QKeyEvent* event) {
 }
 
 void Player::updateState() {
-  velocityY += 1;
+  velocityY += 1.0;
   onGround = false;
   moveBy(0, velocityY);
 
   QList<QGraphicsItem*> items = collidingItems();
 
-  if (items.size() != 0) {
-    QGraphicsItem* item = items[0];
-    setY(item->y() - boundingRect().height());
 
-    velocityY = 0;
-    onGround = true;
-  }
+for (QGraphicsItem* item : items) {
+
+
+        if (velocityY > 0 && (y() + boundingRect().height() - velocityY) <= item->y()) {
+            setY(item->y() - boundingRect().height());
+            velocityY = 0;
+            onGround = true;
+            break; 
+        }
+    }
 }
