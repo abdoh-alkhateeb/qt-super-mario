4cd39c4558975f885996fce5ab2159574639bfbb
christine selwanes <christineselwanes@aucegypt.edu>
2026-04-10 17:22:59 +0000

    Add two platforms, fix player spawn, add You lost! message box

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,33 @@
 #include <QApplication>
-#include <QGraphicsView>
-#include <QGraphicsScene>
 #include <QGraphicsRectItem>
+#include <QGraphicsScene>
+#include <QGraphicsView>
 #include <QTimer>
-#include <QColor>
 #include "player.hpp"
 
 int main(int argc, char* argv[]) {
-    QApplication app(argc, argv);
-
-    QGraphicsScene scene;
-    scene.setSceneRect(0, 0, 600, 400);
-    scene.setBackgroundBrush(QColor(135, 205, 235));
-
-    Player player;
-    scene.addItem(&player);
-
-    QGraphicsRectItem ground(0, 0, 300, 30);
-    ground.setBrush(Qt::darkGreen);
-    ground.setPos(100, 250);
-    scene.addItem(&ground);
+  QApplication app(argc, argv);
   QGraphicsScene scene;
   scene.setSceneRect(0, 0, 2000, 400);
   scene.setBackgroundBrush(QColor(135, 205, 235));
 
-    QGraphicsRectItem platform1(0, 0, 120, 20);
-    platform1.setBrush(Qt::darkGreen);
-    platform1.setPos(420, 200);
-    scene.addItem(&platform1);
+  Player player;
+  scene.addItem(&player);
 
-    QGraphicsRectItem platform2(0, 0, 120, 20);
-    platform2.setBrush(Qt::darkGreen);
-    platform2.setPos(260, 150);
-    scene.addItem(&platform2);
+  QGraphicsRectItem ground(0, 0, 300, 30);
+  ground.setBrush(Qt::darkGreen);
+  ground.setPos(100, 250);
+  scene.addItem(&ground);
+
+  QGraphicsRectItem platform2(0, 0, 200, 20);
+  platform2.setBrush(Qt::darkGreen);
+  platform2.setPos(500, 200);
+  scene.addItem(&platform2);
+
+  QGraphicsRectItem platform3(0, 0, 200, 20);
+  platform3.setBrush(Qt::darkGreen);
+  platform3.setPos(800, 150);
+  scene.addItem(&platform3);
 
   QGraphicsView view(&scene);
   view.setWindowTitle("Qt Super Mario");
@@ -42,9 +37,8 @@ int main(int argc, char* argv[]) {
 
   QTimer timer;
   QObject::connect(&timer, &QTimer::timeout, &player, &Player::updateState);
-  QObject::connect(&timer, &QTimer::timeout,
-                   [&view, &player]() { view.centerOn(&player); });
+  QObject::connect(&timer, &QTimer::timeout, [&view, &player]() { view.centerOn(&player); });
   timer.start(33);
 
-    return app.exec();
+  return app.exec();
 }
--- src/player.cpp
+++ src/player.cpp
@@ -1,50 +1,46 @@
 #include "player.hpp"
-#include <QKeyEvent>
 #include <QBrush>
 #include <QMessageBox>
+#include <QGraphicsScene>
 
 Player::Player(QGraphicsItem* parent)
-    : QObject(), QGraphicsPixmapItem(parent), velocityY(0), onGround(false) {
-
-    // Set the player image
-    setPixmap(QPixmap("assets/player.png"));
-    setPos(300, 0);
-
-    // Make the player focusable
-    setFlag(QGraphicsItem::ItemIsFocusable);
-    setFocus();
+    : QObject(), QGraphicsRectItem(parent), velocityY(0), onGround(false), gameOver(false) {
+  setRect(0, 0, 30, 60);
+  setBrush(Qt::red);
+  setPos(130, 180);
+  setFlag(QGraphicsItem::ItemIsFocusable);
+  setFocus();
 }
 
 void Player::keyPressEvent(QKeyEvent* event) {
-    if (event->key() == Qt::Key_Left) {
-        moveBy(-10, 0);
-    }
-
-    if (event->key() == Qt::Key_Right) {
-        moveBy(10, 0);
-    }
-
-    if (event->key() == Qt::Key_Space && onGround) {
-        velocityY = -15;
-    }
+  if (event->key() == Qt::Key_Left) {
+    moveBy(-10, 0);
+  }
+  if (event->key() == Qt::Key_Right) {
+    moveBy(10, 0);
+  }
+  if (event->key() == Qt::Key_Space && onGround) {
+    velocityY = -15;
+  }
 }
 
 void Player::updateState() {
-    velocityY += 1;
-    onGround = false;
+  if (gameOver) return;
 
-    moveBy(0, velocityY);
+  velocityY += 1;
+  onGround = false;
+  moveBy(0, velocityY);
 
-    QList<QGraphicsItem*> items = collidingItems();
+  QList<QGraphicsItem*> items = collidingItems();
+  if (items.size() != 0) {
+    QGraphicsItem* item = items[0];
+    setY(item->y() - boundingRect().height());
+    velocityY = 0;
+    onGround = true;
+  }
 
-    if (items.size() != 0 && velocityY >= 0) {
-        QGraphicsItem* item = items[0];
-        setY(item->y() - boundingRect().height());
-        velocityY = 0;
-        onGround = true;
-    }
-
-    if (y() > 400) {
-        QMessageBox::information(nullptr, "Game Over", "You lost!");
-    }
+  if (pos().y() > scene()->sceneRect().height()) {
+    gameOver = true;
+    QMessageBox::information(nullptr, "Game Over", "You lost!");
+  }
 }
