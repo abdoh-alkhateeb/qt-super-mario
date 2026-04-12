6fb396153db696cd0295ab8711bef78deedba629
youssef <youssefalfred3012006@gmail.com>
2026-04-12 03:14:46 +0200

    done assignemnt2 :D , add platforms and game over window , improved logic and hopefull will get a bonus from my prof.

--- src/player.cpp
+++ src/player.cpp
@@ -1,12 +1,15 @@
 #include "player.hpp"
 
+#include <QApplication>
+#include <QGraphicsScene>
+#include <QMessageBox>
 #include <QBrush>
 
 Player::Player(QGraphicsItem* parent)
-    : QObject(), QGraphicsRectItem(parent), velocityY(0), onGround(false) {
+    : QObject(), QGraphicsRectItem(parent), velocityY(0), onGround(false), gameOver(false) {
   setRect(0, 0, 30, 60);
   setBrush(Qt::red);
-  setPos(300, 0);
+  setPos(300, 200);
 
   setFlag(QGraphicsItem::ItemIsFocusable);
   setFocus();
@@ -25,17 +28,27 @@ void Player::keyPressEvent(QKeyEvent* event) {
 }
 
 void Player::updateState() {
+  
+
   velocityY += 1;
   onGround = false;
   moveBy(0, velocityY);
 
   QList<QGraphicsItem*> items = collidingItems();
 
-  if (items.size() != 0) {
+  if (items.size() != 0 && velocityY > 0) {// make sure that the rectangle is going donw ( positive y velocity)
     QGraphicsItem* item = items[0];
     setY(item->y() - boundingRect().height());
-
     velocityY = 0;
     onGround = true;
+  }
+
+  if (!gameOver  && y() > scene()->sceneRect().bottom()) {// :D check if the y coordinate of the rectangle is out of the scence bottom y coordinate
+    gameOver = true;
+    QMessageBox* messagebox = new QMessageBox();
+    messagebox->setWindowTitle(":(");
+    messagebox->setText("You lost!");
+    messagebox->setFixedSize(200, 100);
+    messagebox->show();
   }
 }
