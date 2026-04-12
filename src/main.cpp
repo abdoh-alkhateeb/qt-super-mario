b979dc147a9f567ebc239a07f4bd82f3cbf3ef53
Yousuf <yiraky@gmail.com>
2026-04-12 15:44:21 +0000

    Adding two more platforms

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,17 @@ int main(int argc, char* argv[]) {
   ground.setPos(100, 250);
   scene.addItem(&ground);
 
+QGraphicsRectItem platform1(0, 0, 300, 30);
+  platform1.setBrush(Qt::darkGreen);
+  platform1.setPos(500, 200);
+  scene.addItem(&platform1);
+
+QGraphicsRectItem platform2(0, 0, 300, 30);
+  platform2.setBrush(Qt::darkGreen);
+  platform2.setPos(900, 150);
+  scene.addItem(&platform2);
+
+
   QGraphicsView view(&scene);
   view.setWindowTitle("Qt Super Mario");
   view.setFixedSize(640, 480);
