b3ff77f36d23d20d65267698d10f0f55cb41fa92
Moataz Badawy <zezomostafa130@gmail.com>
2026-04-13 01:06:57 +0200

    Add Platforms, merge the image, build and run

--- src/main.cpp
+++ src/main.cpp
@@ -10,24 +10,52 @@ int main(int argc, char* argv[]) {
   QApplication app(argc, argv);
 
   QGraphicsScene scene;
-  scene.setSceneRect(0, 0, 600, 400);
+  scene.setSceneRect(0, 0, 2000, 400);
   scene.setBackgroundBrush(QColor(135, 205, 235));
 
   Player player;
   scene.addItem(&player);
 
+  // First platform
   QGraphicsRectItem ground(0, 0, 300, 30);
   ground.setBrush(Qt::darkGreen);
   ground.setPos(100, 250);
   scene.addItem(&ground);
 
+  // Second platform
+  QGraphicsRectItem* platform1 = new QGraphicsRectItem(0, 0, 250, 30);
+  platform1->setBrush(Qt::darkGreen);
+  platform1->setPos(450, 200);
+  scene.addItem(platform1);
+
+  // Third platform
+  QGraphicsRectItem* platform2 = new QGraphicsRectItem(0, 0, 250, 30);
+  platform2->setBrush(Qt::darkGreen);
+  platform2->setPos(750, 150);
+  scene.addItem(platform2);
+
+  // Fourth platform
+  QGraphicsRectItem* platform3 = new QGraphicsRectItem(0, 0, 200, 30);
+  platform3->setBrush(Qt::darkGreen);
+  platform3->setPos(1100, 220);
+  scene.addItem(platform3);
+
+  // Fifth platform
+  QGraphicsRectItem* platform4 = new QGraphicsRectItem(0, 0, 200, 30);
+  platform4->setBrush(Qt::darkGreen);
+  platform4->setPos(1400, 180);
+  scene.addItem(platform4);
+
   QGraphicsView view(&scene);
   view.setWindowTitle("Qt Super Mario");
-  view.resize(640, 480);
+  view.setFixedSize(640, 480);
+  view.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
   view.show();
 
   QTimer timer;
   QObject::connect(&timer, &QTimer::timeout, &player, &Player::updateState);
+  QObject::connect(&timer, &QTimer::timeout,
+                   [&view, &player]() { view.centerOn(&player); });
   timer.start(33);
 
   return app.exec();
