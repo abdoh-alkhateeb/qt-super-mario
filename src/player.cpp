7807112bf37787c64c9de6e085870c94ce018ba6
yomnaosama-cyber <yomnaosama@aucegypt.edu>
2026-04-12 20:27:27 +0200

    platforms

--- src/player.cpp
+++ src/player.cpp
@@ -1,9 +1,9 @@
 #include "player.hpp"
-
+#include <QDebug>
 #include <QBrush>
 
 Player::Player(QGraphicsItem* parent)
-    : QObject(), QGraphicsRectItem(parent), velocityY(0), onGround(false) {
+    : QObject(), QGraphicsRectItem(parent), velocityY(0), onGround(false), onPlatform(false) {
   setRect(0, 0, 30, 60);
   setBrush(Qt::red);
   setPos(300, 0);
@@ -19,23 +19,34 @@ void Player::keyPressEvent(QKeyEvent* event) {
   if (event->key() == Qt::Key_Right) {
     moveBy(10, 0);
   }
-  if (event->key() == Qt::Key_Space && onGround) {
+  if (event->key() == Qt::Key_Space && (onGround || onPlatform)) {
     velocityY = -15;
+    onGround = false;
+    onPlatform = false;
   }
 }
 
 void Player::updateState() {
   velocityY += 1;
-  onGround = false;
+  if (velocityY > 15) 
+    velocityY = 15;
   moveBy(0, velocityY);
+  onGround = false;
+  onPlatform = false;
 
   QList<QGraphicsItem*> items = collidingItems();
-
-  if (items.size() != 0) {
-    QGraphicsItem* item = items[0];
-    setY(item->y() - boundingRect().height());
-
-    velocityY = 0;
-    onGround = true;
+    
+  for (QGraphicsItem* item : items) {
+    if (item->type() == QGraphicsRectItem::Type) {
+      setY(item->y() - boundingRect().height());
+      velocityY = 0;
+      
+      if (item->y() >= 250) {
+        onGround = true;
+      } else {
+        onPlatform = true;
+      }
+      break;
+    }
   }
 }
