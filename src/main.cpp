4e502005424633405a7ba64e6ec0210402ea3096
Ziad <ziadamr5@aucegypt.edu>
2026-04-13 13:06:59 +0200

    Added platforms the player can jump to included the cmakelist

--- src/main.cpp
+++ src/main.cpp
@@ -3,35 +3,46 @@
 #include <QGraphicsScene>
 #include <QGraphicsView>
 #include <QTimer>
+#include<QMessageBox>
 
 #include "player.hpp"
 
-int main(int argc, char* argv[]) {
-  QApplication app(argc, argv);
 
-  QGraphicsScene scene;
-  scene.setSceneRect(0, 0, 2000, 400);
-  scene.setBackgroundBrush(QColor(135, 205, 235));
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
 
-  Player player;
-  scene.addItem(&player);
+    QGraphicsScene scene;
+    scene.setSceneRect(0, 0, 2000, 400);
+    scene.setBackgroundBrush(QColor(135, 205, 235));
 
-  QGraphicsRectItem ground(0, 0, 300, 30);
-  ground.setBrush(Qt::darkGreen);
-  ground.setPos(100, 250);
-  scene.addItem(&ground);
+    Player player;
+    scene.addItem(&player);
 
-  QGraphicsView view(&scene);
-  view.setWindowTitle("Qt Super Mario");
-  view.setFixedSize(640, 480);
-  view.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-  view.show();
+    QGraphicsRectItem ground(0, 0, 300, 30);
+    QGraphicsRectItem platform(0,0,100,20);
+    QGraphicsRectItem platform2(0,0,90,30);
+    QMessageBox message;
+    ground.setBrush(Qt::darkGreen);
+    platform.setBrush(Qt::yellow);
+    platform2.setBrush(Qt::blue);
+    ground.setPos(100, 250);
+    platform.setPos(510,250);
+    platform2.setPos(10,150);
+    scene.addItem(&ground);
+    scene.addItem(&platform);
+        scene.addItem(&platform2);
 
-  QTimer timer;
-  QObject::connect(&timer, &QTimer::timeout, &player, &Player::updateState);
-  QObject::connect(&timer, &QTimer::timeout,
-                   [&view, &player]() { view.centerOn(&player); });
-  timer.start(33);
+    QGraphicsView view(&scene);
+    view.setWindowTitle("Qt Super Mario");
+    view.setFixedSize(640, 480);
+    view.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    view.show();
 
-  return app.exec();
+    QTimer timer;
+    QObject::connect(&timer, &QTimer::timeout, &player, &Player::updateState);
+    QObject::connect(&timer, &QTimer::timeout, [&view, &player]() { view.centerOn(&player); });
+    timer.start(33);
+
+    return app.exec();
 }
